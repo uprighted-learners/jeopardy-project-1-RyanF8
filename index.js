21f673a51ce1ea6344d1688d8337d8ffa342c3d2
RyanF8 <Ryanfish4229@gmail.com>
2024-03-29 19:26:59 -0400

    moved everything to same folder

---
+++ index.js
@@ -0,0 +1,19 @@
+// import placeholderQuestions from "../placeholder-questions.js";
+
+// let playerOne = playerOne;
+// let playerOneScore = 0;
+// let playerTwo = playerTwo;
+// let playerTwoScore = 0;
+
+const playerTurn = document.getElementById('playerTurn')
+const changeTurn = document.getElementById('pass')
+
+changeTurn.addEventListener('click', () => {
+  playerTurn.textContent = "Player 2's Turn";
+});
+
+
+
+
+
+
