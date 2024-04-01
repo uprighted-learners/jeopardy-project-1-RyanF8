d374a7c9667c52f17f3bd44f4db95a857b59dfdc
RyanF8 <Ryanfish4229@gmail.com>
2024-04-01 13:59:51 -0400

    troubleshooting errors with document in my index.js, not sure what im misssing or where the error lays, notes are in index.js

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
 // import placeholderQuestions from "../placeholder-questions.js";
+// const category = document.querySelector('category')
+// category.push(placeholderQuestions.values(category))
 
 // let playerOne = playerOne;
 // let playerOneScore = 0;
 // let playerTwo = playerTwo;
 // let playerTwoScore = 0;
 
-const playerTurn = document.getElementById('playerTurn')
-const changeTurn = document.getElementById('pass')
+// const playerTurn = document.getElementById('playerTurn')
+// const changeTurn = document.getElementById('pass')
 
-changeTurn.addEventListener('click', () => {
-  playerTurn.textContent = "Player 2's Turn";
-});
+//while player has not answered incorrectly then keep it to player ones turn, else change to player 2's turn
+
+// const newText = document.createElement('p')
+// newText.innerHTML = "player 1 is up";
+
+if (document !== undefined) {
+    console.log('im here!')
+} else {
+    console.log('ERROR ERROR')
+}
+
+//notes for above, crossed out code is layout for later in the process but was trying to use dom to create new elements and keep getting errors where my document is not defined, tried this code above to bug shoot where the problem is but that didnt help as well, any recomendations for trouble shooting this is very helpful, thank you!
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*showing which player turn it is,
+on box click display question,
+await user response on question,
+then check if answer is correct,
+if answer is correct then add points to player count, if wrong subtract points and ask other player the question,
+if pass switch to other player and ask the same question,
+whoever gets the question right picks the next question, 
+when 1 player has 1000 points or board is clear then show next round button.
+
+NEXT ROUND
+repeat round 1 steps.
+
+FINAL JEOPARDY
+ask players how much they want to wager, 
+then display wager amount (think of DOM),
+ask both players to set answers to given question then compare answers if there answer is right then add there wager to points, if wrong then subtract wager from points,
+then declare winner based off points.
+
+-**think about using dom to create elements and appending child**
+*/
